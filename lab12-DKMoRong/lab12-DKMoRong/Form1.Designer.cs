fa6bd0b626ccfb4a8b8ec6c1b088b6a4df49c785
TaDao2501 <91553171+TaDao2501@users.noreply.github.com>
2021-12-03 16:12:06 +0700

    Add files via upload

---
+++ lab12-DKMoRong/lab12-DKMoRong/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace lab12_DKMoRong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.txtso1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtso2 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtkq = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX3 = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(91, 35);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(207, 23);
+            this.labelX1.TabIndex = 0;
+            this.labelX1.Text = "CHƯƠNG TRÌNH TÍNH TỔNG HAI SỐ";
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(42, 93);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(107, 23);
+            this.labelX2.TabIndex = 1;
+            this.labelX2.Text = "Nhập Số Thứ Nhất";
+            // 
+            // labelX4
+            // 
+            // 
+            // 
+            // 
+            this.labelX4.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX4.Location = new System.Drawing.Point(42, 178);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(75, 23);
+            this.labelX4.TabIndex = 3;
+            this.labelX4.Text = "Kết Qủa";
+            // 
+            // txtso1
+            // 
+            // 
+            // 
+            // 
+            this.txtso1.Border.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtso1.Border.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtso1.Border.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtso1.Border.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtso1.Border.Class = "TextBoxBorder";
+            this.txtso1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Diagonal;
+            this.txtso1.Location = new System.Drawing.Point(198, 93);
+            this.txtso1.Name = "txtso1";
+            this.txtso1.Size = new System.Drawing.Size(100, 28);
+            this.txtso1.TabIndex = 4;
+            // 
+            // txtso2
+            // 
+            // 
+            // 
+            // 
+            this.txtso2.Border.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtso2.Border.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtso2.Border.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtso2.Border.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtso2.Border.Class = "TextBoxBorder";
+            this.txtso2.Border.CornerType = DevComponents.DotNetBar.eCornerType.Diagonal;
+            this.txtso2.Location = new System.Drawing.Point(198, 137);
+            this.txtso2.Name = "txtso2";
+            this.txtso2.Size = new System.Drawing.Size(100, 28);
+            this.txtso2.TabIndex = 6;
+            // 
+            // txtkq
+            // 
+            // 
+            // 
+            // 
+            this.txtkq.Border.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtkq.Border.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtkq.Border.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtkq.Border.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Double;
+            this.txtkq.Border.Class = "TextBoxBorder";
+            this.txtkq.Border.CornerType = DevComponents.DotNetBar.eCornerType.Diagonal;
+            this.txtkq.Location = new System.Drawing.Point(198, 178);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(100, 28);
+            this.txtkq.TabIndex = 7;
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Location = new System.Drawing.Point(42, 137);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(107, 23);
+            this.labelX3.TabIndex = 10;
+            this.labelX3.Text = "Nhập Số Thứ Hai";
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(61, 220);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 11;
+            this.buttonX1.Text = "Tổng";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(170, 220);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(75, 23);
+            this.buttonX2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX2.TabIndex = 12;
+            this.buttonX2.Text = "Nhập Lại";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // buttonX3
+            // 
+            this.buttonX3.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX3.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX3.Location = new System.Drawing.Point(273, 219);
+            this.buttonX3.Name = "buttonX3";
+            this.buttonX3.Size = new System.Drawing.Size(75, 23);
+            this.buttonX3.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX3.TabIndex = 13;
+            this.buttonX3.Text = "Thoát";
+            this.buttonX3.Click += new System.EventHandler(this.buttonX3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(385, 274);
+            this.Controls.Add(this.buttonX3);
+            this.Controls.Add(this.buttonX2);
+            this.Controls.Add(this.buttonX1);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txtso2);
+            this.Controls.Add(this.txtso1);
+            this.Controls.Add(this.labelX4);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.labelX1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtso1;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtso2;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtkq;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private DevComponents.DotNetBar.ButtonX buttonX3;
+    }
+}
+
---
+++ lab12-DKMoRong/lab12-DKMoRong/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab12_DKMoRong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonX3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            txtso1.Text = "";
+            txtso2.Clear();
+            txtkq.Clear();
+        }
+
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            string soa = txtso1.Text; 
+            string sob = txtso2.Text; 
+            if (soa.Length == 0 || sob.Length == 0)
+            {
+                MessageBox.Show("Vui lòng điền đủ cả 2 ô số nguyên A và B để có thể thực hiện phép tính.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                float a = float.Parse(soa);
+                float b = float.Parse(sob);
+                float result = a + b; 
+                txtkq.Text = result.ToString();
+               
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
